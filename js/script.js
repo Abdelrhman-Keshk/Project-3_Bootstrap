9ac87f0e94176b3219a8802a181a86fa0fe86c9f
Abdelrhmankeshk <abdelrhmankeshk15@gmail.com>
2024-06-02 02:16:06 +0300

    create priject

---
+++ js/script.js
@@ -0,0 +1,19 @@
+var copyTranBtn = document.getElementById("copyTranBtn");
+var copyTranBtn2 = document.getElementById("copyTranBtn2");
+var copyTranBtn3 = document.getElementById("copyTranBtn3");
+
+function copyAlert() {
+  alert("تم نسخ النص");
+}
+
+copyTranBtn.onclick = copyAlert;
+copyTranBtn2.onclick = copyAlert;
+copyTranBtn3.onclick = copyAlert;
+
+const popoverTriggerList = document.querySelectorAll('[data-bs-toggle="popover"]')
+const popoverList = [...popoverTriggerList].map(popoverTriggerEl => new bootstrap.Popover(popoverTriggerEl))
+
+const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
+const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
+
+
