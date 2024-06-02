2e8a80bbe191f2f96f870f9c64a507b6621864e9
Abdelrhmankeshk <abdelrhmankeshk15@gmail.com>
2024-06-02 23:19:43 +0300

    Tricks update2

--- js/script.js
+++ js/script.js
@@ -15,5 +15,3 @@ const popoverList = [...popoverTriggerList].map(popoverTriggerEl => new bootstra
 
 const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
 const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
-
-
